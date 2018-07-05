6fadf95a909c98b8e5cd04ddef98318f1552c18c
wanglin <wanglin@zhongan.io>
2018-07-05 13:37:38 +0800

    侧边栏暂存

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -5,7 +5,7 @@
       :show-timeout="200"
       :default-active="$route.path"
       :collapse="isCollapse"
-      background-color="#304156"
+      background-color="#001529"
       text-color="#bfcbd9"
       active-text-color="#409EFF"
     >
